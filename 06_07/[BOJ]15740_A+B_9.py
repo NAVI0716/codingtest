bb609ab24348b4c3403af48c6119f8030f59958b
Ryu seong bin <tjdqls0716@gmail.com>
2023-06-07 16:47:30 +0900

    2023_0607

---
+++ 06_07/[BOJ]15740_A+B_9.py
@@ -0,0 +1,5 @@
+a,b=map(int,input().split())
+# if (A > -10^10000 and A < 10^10000) and (B > -10^10000 and B < 10^10000):
+#     print(a+b)
+print(abs(a))
+    
---
+++ 06_07/[BOJ]16170_오늘의날짜는.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+print(datetime.utcnow().strftime('%Y'))
+print(datetime.utcnow().strftime('%m'))
+print(datetime.utcnow().strftime('%d'))
+# for i in range(3):
+#     print(datetime.utcnow()[0][i])
+# print(datetime.today().strftime("%Y-%m-%d"))
